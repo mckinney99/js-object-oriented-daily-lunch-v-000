09d649211ab519c061caaef44687d27c5fab498f
Eric McKinney <mckinney99@gmail.com>
2018-02-28 15:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let store = {customers: [], meals: [], deliveries: [], employers: []}
 
-let customers = 0
+let customerId = 0
 let mealId = 0
 let deliveryId = 0
 let employerId = 0
 
 class Customer {
     constructor(name, employer) {
-        this.id = ++customers
+        this.id = ++customerId
         this.name = name
         this.employer = employer
         store.customers.push(this)
